0f80c9859aa7d1d4dbf85bb033922e5c21c731ec
Avijit pal <avijitpal4343@gmail.com>
2021-03-16 01:20:07 +0530

    updated

--- app.js
+++ app.js
@@ -2,16 +2,25 @@
 const express = require('express');
 const app = express();
 const port = 2766;
+var mongoDbUrl = "mongodb://localhost:27017/Time-table";
+let mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/Time-table", 
+{ useUnifiedTopology: true, useNewUrlParser: true });
+
+mongoose.connection.once('open', function(){
+    console.log('connection done');
+}).on('error',function(error){
+    console.log('error is',error)
+});
 
 
-
+//imorting modules 
 
 app.get("/",(req,res) => {
     console.log("hello");
     res.send("hello im on");
-})
-
-
+})  
 app.use("/users",(req,res,next)=>{
     var name = {firstname: "avijit",lastname:"pal"}
     res.json([name])
@@ -19,7 +28,12 @@ app.use("/users",(req,res,next)=>{
 })
 
 // starting the server
-
 app.listen(port, function () {
         console.log("hello the server is on");
     })
+
+
+    module.exports =
+    {
+      
+    }
---
+++ mongoDB.js
@@ -0,0 +1,26 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/Time-table";
+const mongoose = require("mongoose");
+
+MongoClient.connect(url,function(err,db){
+    console.log("database is connected");
+    db.close();
+});
+
+
+
+
+//(function updateUserdataToDb(jsonData) {
+    
+   // var newValues = {$set : jsonData} 
+ //   MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
+  //      if (err) throw err;
+   //     var dbo = db.db("mainDb");
+     //   dbo.collection("userData").updateOne({"userId": jsonData["userId"]}, newValues, {upsert: true}, function(err, res) {
+ //           if (err) throw err;
+   //         console.log("userData updated to ", jsonData);
+    //        db.close();
+//        });
+   // }); 
+
+
