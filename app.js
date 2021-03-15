df0c24ff4af20b849dd1b9c5eb15185e2ec48364
Avijit pal <avijitpal4343@gmail.com>
2021-03-16 02:21:49 +0530

    new updation

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // load or create a server 
 const express = require('express');
+var MongoClient = require('mongodb').MongoClient;
 const app = express();
 const port = 2766;
 var mongoDbUrl = "mongodb://localhost:27017/Time-table";
 let mongoose = require('mongoose');
+var bodyParser=require("body-parser");
 
 mongoose.connect("mongodb://localhost:27017/Time-table", 
 { useUnifiedTopology: true, useNewUrlParser: true });
@@ -32,8 +34,19 @@ app.listen(port, function () {
         console.log("hello the server is on");
     })
 
+app.post('/title', function(req,res){
+  var title = req.body.title;
+ mongoose.Collection('Title').insertOne(title,function(err, collection){
+   if(err) throw err;
+   console.log("added in the databse");
+ });
+})
 
-    module.exports =
-    {
-      
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res) {
+    res.redirect('index.html');
+});
+
+    module.exports = {
+
     }
