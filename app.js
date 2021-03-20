c905719b38e85b1108f1fcef7f0d4ea600b4a75a
Avijit pal <avijitpal4343@gmail.com>
2021-03-21 01:41:59 +0530

    database is connected

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require('express');
 var port = 2777;
 var bodyParser = require('body-parser');
 var app = express();
-
+var MongoClient = require('mongodb').MongoClient;
+//const { Db } = require('mongodb');
+var url= "mongodb://localhost:27017/Time-table";
 app.use(bodyParser.urlencoded({extended:false})); 
 app.use(bodyParser.json());
 
@@ -13,17 +15,28 @@ app.listen(port,()=>{
     console.log("server is up");
 })
 
-
 //app.use(express)
 //getting the title
-app.post('/title',function(req,res){
-    title={
+app.post('/eventdata',function(req,res){
+    data={
              title:req.body.title,
+             event:req.body.event,
     };
     
-    console.log(JSON.stringify(title));
+    console.log(JSON.stringify(data));
 })
 
+MongoClient.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true }, function (err, db) {
+   if (err) 
+   	throw err
+   else
+   {
+	console.log('Connected to MongoDB');
+	//Start app only after connection is ready
+	app.listen(3000);
+    db.close();
+   }
+ });
 
 //will load the html file and so on
 app.use(express.static(__dirname + '/public'));
