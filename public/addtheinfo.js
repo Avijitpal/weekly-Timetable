a688a4c70ab23c7eef495f93015129264d691584
Avijit pal <avijitpal4343@gmail.com>
2021-04-06 20:41:25 +0530

    table added

--- public/addtheinfo.js
+++ public/addtheinfo.js
@@ -7,27 +7,25 @@
 
 
 function insertData() {
-     var title = document.getElementById("Title").value;
-     var event= document.getElementById("Event").value;
-     var time = document.getElementById("Time").value;
-     var day = document.getElementById("week-days").value;
+  var title = document.getElementById("Title").value;
+  var event= document.getElementById("Event").value;
+  var time = document.getElementById("Time").value;
+  var day = document.getElementById("week-days").value;
      
-     document.getElementById("insertionPoint").innerHTML += "<tr><td>"
-     + title +time+day+"</td><td>"+
-     
-       
-                              "<td></td>"+
-                              "<td></td>"+
-                              "<td></td>"+
-                              "<td></td>"+
-                              "<td></td>"+
+  document.getElementById("insertionPoint").innerHTML += "<tr><td>"
+         + title +time+day+"</td><td>"+
+                           "<td></td>"+
+                           "<td></td>"+
+                           "<td></td>"+
+                           "<td></td>"+
+                           "<td></td>"+
+                           "</td></tr>";
 
-               "</td></tr>"
+
    // event + "</td></tr>";
   
     // The below part is to clear the values after the entry is added.
-   //document.getElementById("Title").value = ""
-   //document.getElementById("Event").value = "";
+   //document.getElementById("Title") = ""
+   //document.getElementById("Event") = "";
 }
-
 
