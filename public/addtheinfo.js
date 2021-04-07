8da31dc6802669ae05782927acc39eb2d7dc6901
Avijit pal <avijitpal4343@gmail.com>
2021-04-07 16:13:50 +0530

    table updated

--- public/addtheinfo.js
+++ public/addtheinfo.js
@@ -11,21 +11,35 @@ function insertData() {
   var event= document.getElementById("Event").value;
   var time = document.getElementById("Time").value;
   var day = document.getElementById("week-days").value;
-     
-  document.getElementById("insertionPoint").innerHTML += "<tr><td>"
-         + title +time+day+"</td><td>"+
-                           "<td></td>"+
-                           "<td></td>"+
-                           "<td></td>"+
-                           "<td></td>"+
-                           "<td></td>"+
-                           "</td></tr>";
-
-
-   // event + "</td></tr>";
+  if(day == "Monday")
+  {
+    console.log("its monday");
+  }
+  if(day == "Tuesday")
+  {
+     console.log("its tuesday");
+  }
   
-    // The below part is to clear the values after the entry is added.
-   //document.getElementById("Title") = ""
-   //document.getElementById("Event") = "";
+  document.getElementById("table").innerHTML += "<tr>"
+  +"<td>"+day+"</td>"
+  +"<td>"+day+"</td>"
+  + "<td>"+day+"</td>"
+  + "<td>"+day+"</td>"
+  + "<td>"+day+"</td>"
+  + "<td>"+day+"</td>"
+  + "<td>"+day+"</td>"
+  
+                                              "</tr>";
+
+
+// event + "</td></tr>";
+
+// The below part is to clear the values after the entry is added.
+//document.getElementById("Title") = ""
+//document.getElementById("Event") = "";
 }
+
+
+     
+
 
